7b713043904dca3f627f12c1c54fba88e59851b1
AIM <rolety2202@naver.com>
2025-02-24 17:16:39 +0900

    UI 이론 -Frame -Gravity -ScrollView -resource -drawable -서브웨이 로그인 화면 그려보기

--- app/src/main/java/com/example/fastcampus/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 //        setContentView(R.layout.activity_main)
-        setContentView(R.layout.linearlayout)
+        setContentView(R.layout.drawable)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
