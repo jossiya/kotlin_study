aff4603d9dcb71e0282d58c4dc34dd03afcdeea4
AIM <rolety2202@naver.com>
2025-03-06 11:00:22 +0900

    컨플릭트 제거

--- app/src/main/java/com/example/fastcampus/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus/MainActivity.kt
@@ -11,11 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 //        setContentView(R.layout.activity_main)
-<<<<<<< HEAD
         setContentView(R.layout.activity_main)
-=======
-        setContentView(R.layout.homework_1)
->>>>>>> e86c4899e1a1cdfeb4e7f2df423b0e229fcd6089
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
