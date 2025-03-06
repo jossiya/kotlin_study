941d118eb1fc2db612d3a0cb196468d3550ee1ca
AIM <rolety2202@naver.com>
2025-03-06 13:56:25 +0900

    Activity -ViewControl

--- app/src/main/java/com/example/fastcampus/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fastcampus
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +11,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d("LifeCycle", "onCreate")
+    }
+
+    override fun onStart() {
+        Log.d("LifeCycle", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("LifeCycle", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("LifeCycle", "onRestart")
+        super.onRestart()
     }
 }
