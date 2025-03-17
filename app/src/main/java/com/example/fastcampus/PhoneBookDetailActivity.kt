849eaec2c6a7759385743cd4d4d76ce249d62d26
AIM <rolety2202@naver.com>
2025-03-17 16:25:55 +0900

    -addView

---
+++ app/src/main/java/com/example/fastcampus/PhoneBookDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.example.fastcampus
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PhoneBookDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_phone_book_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val name: String = intent.extras?.getString("name")?:""
+        val number: String = intent.extras?.getString("number")?:""
+
+        findViewById<TextView>(R.id.name).text = name
+        findViewById<TextView>(R.id.number).text = number
+
+        
+    }
+}
