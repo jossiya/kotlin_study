726e527d928067a7a61af167eb7e80cbb251361f
AIM <rolety2202@naver.com>
2025-03-17 11:58:35 +0900

    -Library

---
+++ app/src/main/java/com/example/fastcampus/GlideActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fastcampus
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+
+class GlideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_glide)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val imageView = findViewById<ImageView>(R.id.image)
+        Glide.with(this)
+            .load("https://placehold.co/600x400/000000/FFFFFF/png")
+            .centerCrop()
+            .into(imageView)
+    }
+}
