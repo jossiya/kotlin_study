b8317344e336eab9efee4c5e162b6a4995e1d1ee
AIM <rolety2202@naver.com>
2025-03-20 18:00:53 +0900

    -RETROFIT -create

--- app/src/main/java/com/example/fastcampus/RetrofitActivity.kt
+++ app/src/main/java/com/example/fastcampus/RetrofitActivity.kt
@@ -1,18 +1,24 @@
 package com.example.fastcampus
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
+import retrofit2.Retrofit.Builder
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 class RetrofitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +30,7 @@ class RetrofitActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val retrofit = Retrofit.Builder()
+        val retrofit = Builder()
             .baseUrl("http://mellowcode.org/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -36,8 +42,9 @@ class RetrofitActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         val students = response.body()
                         withContext(Dispatchers.Main) {
-                            students!!.forEach {
-                                Log.d("RetrofitTest", "ID: ${it.id}, Name: ${it.name}, Age: ${it.age}")
+                            findViewById<RecyclerView>(R.id.studentsRecyclerview).apply{
+                                this.adapter = StudentListRecyclerViewAdapter(students!!, LayoutInflater.from(this@RetrofitActivity) )
+                                this.layoutManager = LinearLayoutManager(this@RetrofitActivity)
                             }
                         }
                     }else{
@@ -48,5 +55,79 @@ class RetrofitActivity : AppCompatActivity() {
                     Log.e("RetrofitTest", "Error: ${e.message}")
                 }
             }
+        findViewById<TextView>(R.id.studentCreate).setOnClickListener{
+            val student = HashMap<String, Any>()
+            student["name"] = "조철호"
+            student["age"] =40
+            student["intro"] = "코틀린 빡세요"
+            CoroutineScope(Dispatchers.IO).launch{
+                val response = retrofit.createStudent(student)
+                try{
+                    if (response.isSuccessful) {
+                        withContext(Dispatchers.Main) {
+                            Log.e("RetrofitTest", "성공: ${response.body()}")
+                        }
+                    }else{
+                        Log.e("RetrofitTest", "Error Code: ${response.code()}")
+                    }
+                }catch (e : Exception){
+                    e.printStackTrace()
+                    Log.e("RetrofitTest", "Error: ${e.message}")
+                }
+            }
+        }
+        findViewById<TextView>(R.id.easyStudentCreate).setOnClickListener {
+            val student = StudentFromServer(name = "하나", age = 100, "hello kotlin" )
+            CoroutineScope(Dispatchers.IO).launch{
+                val response = retrofit.easyCreateStudent(student)
+                try{
+                    if (response.isSuccessful) {
+                        withContext(Dispatchers.Main) {
+                            Log.e("RetrofitTest", "성공: ${response.body()}")
+                        }
+                    }else{
+                        Log.e("RetrofitTest", "Error Code: ${response.code()}")
+                    }
+                }catch (e : Exception){
+                    e.printStackTrace()
+                    Log.e("RetrofitTest", "Error: ${e.message}")
+                }
+            }
+        }
+
     }
+}
+class StudentListRecyclerViewAdapter(
+    var studentList: List<StudentFromServer>,
+    var inflater: LayoutInflater
+): RecyclerView.Adapter<StudentListRecyclerViewAdapter.ViewHolder>(){
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val studentName: TextView
+        val studentAge: TextView
+        val studentIntro: TextView
+
+        init{
+            studentName = itemView.findViewById(R.id.studentName)
+            studentAge = itemView.findViewById(R.id.studentAge)
+            studentIntro = itemView.findViewById(R.id.studentIntro)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.student_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.studentName.text = studentList[position].name
+        holder.studentAge.text = studentList[position].age.toString()
+        holder.studentIntro.text = studentList[position].intro
+    }
+
+
+    override fun getItemCount(): Int {
+        return studentList.size
+    }
+
+
 }
--- app/src/main/java/com/example/fastcampus/RetrofitService.kt
+++ app/src/main/java/com/example/fastcampus/RetrofitService.kt
@@ -1,12 +1,20 @@
 package com.example.fastcampus
 
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 data class StudentFromServer(
     val id: Int, val name: String, val age: Int, val intro: String
-)
+){
+    constructor( name: String, age: Int, intro: String):this(0, name, age, intro)
+}
 interface RetrofitService {
     @GET("json/students/")
     suspend fun getStudentList():Response<List<StudentFromServer>>
+    @POST("json/students/")
+    suspend fun createStudent(@Body params:HashMap<String, Any>):Response<StudentFromServer>
+    @POST("json/students/")
+    suspend fun easyCreateStudent(@Body params:StudentFromServer):Response<StudentFromServer>
 }
